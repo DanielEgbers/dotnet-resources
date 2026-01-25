e4938f7f2035e8f81283265fae7fca704cb23fbf
Daniel Egbers <daniel@egbers.site>
2026-01-25 01:30:22 +0100

    update System.CommandLine from beta to 2.0.2

--- dotnet-resources/Program.cs
+++ dotnet-resources/Program.cs
@@ -4,119 +4,118 @@ using System.IO;
 using System.Threading.Tasks;
 using CliWrap.Exceptions;
 
-namespace Dotnet.Resources
+return await BuildRootCommand().Parse(args).InvokeAsync();
+
+RootCommand BuildRootCommand()
 {
-    public static class Program
+    var rootCommand = new RootCommand();
+
     {
-#pragma warning disable IDE1006 // Naming Styles
-        public static async Task<int> Main(string[] args)
-#pragma warning restore IDE1006 // Naming Styles
+        var command = new Command("versioninfo");
+
+        rootCommand.Add(command);
+
         {
-            var versionInfoCopySourceFileArgument = new Argument<FileInfo>("source-file");
-            var versionInfoCopyTargetFileArgument = new Argument<FileInfo>("target-file");
-            var versionInfoCopyCommand = new Command("copy")
-            {
-                versionInfoCopySourceFileArgument,
-                versionInfoCopyTargetFileArgument,
-            };
-            versionInfoCopyCommand.SetHandler
-            (
-                (FileInfo sourceFile, FileInfo targetFile) =>
-                {
-                    return ExecVersionInfoCopyAsync(sourceFile.FullName, targetFile.FullName);
-                },
-                versionInfoCopySourceFileArgument,
-                versionInfoCopyTargetFileArgument
+            var sourceFileArgument = new Argument<FileInfo>("source-file");
+            var targetFileArgument = new Argument<FileInfo>("target-file");
 
-            );
-
-            var versionInfoCommand = new Command("versioninfo")
+            var subCommand = new Command("copy")
             {
-                versionInfoCopyCommand
+                sourceFileArgument,
+                targetFileArgument,
             };
 
-            var rootCommand = new RootCommand()
+            subCommand.SetAction((ParseResult parseResult) =>
             {
-                versionInfoCommand
-            };
+                var sourceFile = parseResult.GetRequiredValue(sourceFileArgument);
+                var targetFile = parseResult.GetRequiredValue(targetFileArgument);
 
-            return await rootCommand.InvokeAsync(args);
+                return ExecVersionInfoCopyAsync(sourceFile.FullName, targetFile.FullName);
+            });
+
+            command.Add(subCommand);
         }
+    }
 
-        private static async Task<int> ExecVersionInfoCopyAsync(string sourceFile, string targetFile)
+    return rootCommand;
+}
+
+async Task<int> ExecVersionInfoCopyAsync(string sourceFile, string targetFile)
+{
+    if (!File.Exists(sourceFile))
+    {
+        Console.Error.WriteLine("ERROR: source file not found");
+        return 2;
+    }
+
+    if (!File.Exists(targetFile))
+    {
+        Console.Error.WriteLine("ERROR: target file not found");
+        return 3;
+    }
+
+    try
+    {
+        var resourceHacker = CliWrap.Cli.Wrap(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ResourceHacker.exe"));
+
+        var rcFile = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName() + ".rc");
+
+        await resourceHacker
+            .WithArguments(args => args
+                .Add("-open").Add(sourceFile)
+                .Add("-save").Add(rcFile)
+                .Add("-action").Add("extract")
+                .Add("-mask").Add("VersionInfo")
+            )
+            .ExecuteAsync();
+        try
         {
-            if (!File.Exists(sourceFile))
-            {
-                Console.Error.WriteLine("ERROR: source file not found");
-                return 2;
-            }
+            var resFile = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName() + ".res");
 
-            if (!File.Exists(targetFile))
-            {
-                Console.Error.WriteLine("ERROR: target file not found");
-                return 3;
-            }
-
+            await resourceHacker
+                .WithArguments(args => args
+                    .Add("-open").Add(rcFile)
+                    .Add("-save").Add(resFile)
+                    .Add("-action").Add("compile")
+                )
+                .ExecuteAsync();
             try
             {
-                var resourceHacker = CliWrap.Cli.Wrap(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ResourceHacker.exe"));
-
-                var rcFile = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName() + ".rc");
+                await resourceHacker
+                    .WithArguments(args => args
+                        .Add("-open").Add(targetFile)
+                        .Add("-save").Add(targetFile)
+                        .Add("-res").Add(resFile)
+                        .Add("-action").Add("delete")
+                        .Add("-mask").Add("VersionInfo")
+                    )
+                    .ExecuteAsync();
 
                 await resourceHacker
                     .WithArguments(args => args
-                        .Add("-open").Add(sourceFile)
-                        .Add("-save").Add(rcFile)
-                        .Add("-action").Add("extract")
+                        .Add("-open").Add(targetFile)
+                        .Add("-save").Add(targetFile)
+                        .Add("-res").Add(resFile)
+                        .Add("-action").Add("addoverwrite")
                         .Add("-mask").Add("VersionInfo")
-                    ).ExecuteAsync();
-                try
-                {
-                    var resFile = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName() + ".res");
-
-                    await resourceHacker
-                        .WithArguments(args => args
-                            .Add("-open").Add(rcFile)
-                            .Add("-save").Add(resFile)
-                            .Add("-action").Add("compile")
-                        ).ExecuteAsync();
-                    try
-                    {
-                        await resourceHacker
-                            .WithArguments(args => args
-                                .Add("-open").Add(targetFile)
-                                .Add("-save").Add(targetFile)
-                                .Add("-res").Add(resFile)
-                                .Add("-action").Add("delete")
-                                .Add("-mask").Add("VersionInfo")
-                            ).ExecuteAsync();
-
-                        await resourceHacker
-                            .WithArguments(args => args
-                                .Add("-open").Add(targetFile)
-                                .Add("-save").Add(targetFile)
-                                .Add("-res").Add(resFile)
-                                .Add("-action").Add("addoverwrite")
-                                .Add("-mask").Add("VersionInfo")
-                            ).ExecuteAsync();
-                    }
-                    finally
-                    {
-                        File.Delete(resFile);
-                    }
-                }
-                finally
-                {
-                    File.Delete(rcFile);
-                }
-
-                return 0;
+                    )
+                    .ExecuteAsync();
             }
-            catch (CommandExecutionException)
+            finally
             {
-                Console.Error.WriteLine("ERROR: unknown");
-                return 1;
+                File.Delete(resFile);
             }
         }
+        finally
+        {
+            File.Delete(rcFile);
+        }
+
+        return 0;
+    }
+    catch (CommandExecutionException)
+    {
+        Console.Error.WriteLine("ERROR: unknown");
+        return 1;
     }
 }
